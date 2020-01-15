2c9817aac97e5e3a188b477cd984eddbfd26820b
aJlax <58111594+aJlax@users.noreply.github.com>
2020-01-15 17:47:38 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -25,26 +25,29 @@ class ResourceManager:
         return self._resources[name]
 
 
-class Tile(pygame.sprite.Sprite):
+class Tile:
 
     def __init__(self, tile_type, pos_x, pos_y, resource_manager):
         super().__init__(self.tiles_group, self.all_sprites)
+        self.sprite = pygame.sprite.Sprite()
+        self.type = tile_type
+        self.sprite.image = resource_manager.get_resourse(tile_type)
+        self.sprite.rect = self.sprite.image.get_rect()
+        self.sprite.rect = self.sprite.rect.move(pos_x, pos_y)
 
-    def draw(self, surf):
-        pass
 
-    all_sprites = pygame.sprite.Group()
-    tiles_group = pygame.sprite.Group()
+    def get_sprite(self):
+        return self.sprite
 
 
-class Board:
+class GameWorld:
 
     def __init__(self, width, height, x=10, y=10, cell_size=30):
         self._width = width
         self._height = height
         self._x = x
         self._y = y
-        self._board = [[Tile() for i in range(height)] for _ in range(width)]
+        self._board = [[None for i in range(height)] for _ in range(width)]
         self._cell_size = cell_size
 
     def get_cell(self, mouse_pos):
@@ -52,7 +55,7 @@ class Board:
         cell_pos = (mp[0] // self._cell_size, mp[1] // self._cell_size)
         if not (cell_pos[0] < 0 or cell_pos[0] >= self._width or
                 cell_pos[1] < 0 or cell_pos[1] >= self._height):
-                return cell_pos
+            return cell_pos
         return None
 
     def on_click(self, cell):
@@ -63,23 +66,32 @@ class Board:
         if cell is not None:
             self.on_click(cell)
 
+    def load_world(self, name, resourse_manager):
+        for x in range(self._height):
+            for y in range(self._width):
+                if name[y][x] == '.':
+                    Tile('sand', self._x, self._y, resourse_manager)
+                elif name[y][x] == '#':
+                    Tile('tree', self._x, self._y, resourse_manager)
+                elif name[y][x] == '&':
+                    Tile('grass', self._x, self._y, resourse_manager)
+                elif name[y][x] == '%':
+                    Tile('tron', self._x, self._y, resourse_manager)
+
     def draw(self, surf):
         for i in range(self._width):
             for j in range(self._height):
                 pygame.draw.rect(surf, pygame.Color('white'), (self._x + i * self._cell_size,
                                                                self._y + j * self._cell_size,
-                                                               self._cell_size, self._cell_size),
-                                1 - self._board[i][j])
+                                                               self._cell_size, self._cell_size))
 
 
 pygame.init()
 screen = pygame.display.set_mode(DISPLAY_SIZE)
 
-board = Board(40, 40, cell_size=30)
+board = GameWorld(40, 40, cell_size=30)
 
 tile_width = tile_height = 50
-
-
 
 
 running = True
